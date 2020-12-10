ab5b3ba729fe46f787f449444811312ebd37eaff
Grant <grantferment@gmail.com>
2020-12-10 14:48:56 -0500

    seriously, initial commit take 4

--- server.js
+++ server.js
@@ -1,32 +1,25 @@
-const mysql = require("mysql");
-const express = require("express")
-const app = express();
-
-const PORT = process.env.PORT || 8080
-
-let connection; 
-
+var mysql = require("mysql");
+​
+var connection;
+​
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else{
-    connection = mysql.createConnection({
-        host:"localhost",
-        port: 3306,
-        user: "root",
-        password: "",
-        database: "wizard_schools_db"
-    })
-};
-
-connection.connect(function(err){
-    if (err) throw err;
-    console.log("CONNECTED!");
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+}
+​
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
-
-app.get("/", function(req, res){
-    res.send("CONNECTED!")
-});
-
-app.listen(PORT, function(){
-    console.log("you've connected at http://localhost:" + PORT)
-})
+​
+module.exports = connection;
