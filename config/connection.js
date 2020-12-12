e591a6511bd8adee9ee8a133e5b60f1425d62a2e
Grant <grantferment@gmail.com>
2020-12-12 13:05:28 -0500

    changing connections

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,19 @@
-// Set up MySQL connection.
 var mysql = require("mysql");
-var PORT = process.env.PORT || 8080
-
+​
 var connection;
- if(process.env.JAWSDB_URL){
-   connection = mysql.createConnection(process.env.JAWSDB_URL)
- } 
- else {
-  conneciton = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "daBurger_db"
-});
-};
-
-// Make connection.
+​
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+}
+​
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -24,6 +21,5 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
-
-// Export connection for our ORM to use.
+​
 module.exports = connection;
